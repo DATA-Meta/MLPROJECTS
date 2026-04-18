f491a5b450bb80ea26fc9b01f864163b53c3d308
data_holick <111300107+DATA-Meta@users.noreply.github.com>
2026-04-18 21:36:45 +0200

    First Setup Comitt

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## End To End Data Science Project
+## DATA SCIENCE PROJECTS
+## added libraries by creating panas and numpy in requirements.txt
+## setup,py file created , purpose is to decribe the model distribution
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+    return requirements
+
+setup(
+    name="MLProject",
+    version="0.0.1",
+    author="Usman",
+    author_email="usman.5154@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=get_requirements("requirements.txt")
+)
